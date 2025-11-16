207332aea16254f21f3a91419e5d0d53c1cbc54d
rezaroshan100178 <rezaroshan100178@gmail.com>
2025-11-16 14:14:50 +0330

    config env

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const {DataTypes, Op, Sequelize} = require('sequelize')
 
-const sequelize = new Sequelize('feedbackSite', 'root', 'password', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false
 })
